3d98229025a7093c00aa782f0cffeb9481beb5c7
Victoria <veejo90@gmail.com>
2022-01-28 11:10:06 -0500

    Connect 4 bones#

--- arcade/app.js
+++ arcade/app.js
@@ -1,3 +1,20 @@
+
+
+
+const board = [];
+
+for (let i = 0; i < 7; i++) {
+  board.push([])
+}
+
+
+/* keep track of the players and the board
+const gameState = {
+    board: board, // from above
+    players: ['red', 'yellow'] // or whatever
+  }
+
+*/
 const board = document.getElementById("board");
 const cookieCountEle = document.getElementById("cookieCount");
 let state; 
